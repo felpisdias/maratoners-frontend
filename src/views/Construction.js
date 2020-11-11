8e54340ab4ac7eca4f84bf1a84d64ce5784adecd
Felipe Dias <felipedias@pop-os.localdomain>
2020-11-10 21:03:21 -0300

    construction view

---
+++ src/views/Construction.js
@@ -0,0 +1,16 @@
+import './../styles/Form.css';
+import './../styles/Construction.css';
+
+import React from 'react';
+
+export default function Construction() {
+	return (
+		<div id="Form">
+			<strong id="title">
+				Ainda em desenvolvimento
+			</strong>
+			<div className="construction">
+            </div>
+		</div>
+	);
+};
